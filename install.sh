cbdfd50a62acf0ba27f24380a1c38c572c70c7c1
Fedor Scholz <scholz@fzi.de>
2018-05-16 15:44:00 +0200

    only add model to list if not already present

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ DIR=$PWD
 cd $HBP/Models/CDP4_models/
 
 for p in *_poster; do
-  echo "CDP4_models/$p" >> ../_rpmbuild/models.txt
+  already_present=`grep "CDP4_models/$p" ../_rpmbuild/models.txt`
+  if [ -z "$already_present" ]; then
+    echo "CDP4_models/$p" >> ../_rpmbuild/models.txt
+  fi
 done
 
 ../create-symlinks.sh
